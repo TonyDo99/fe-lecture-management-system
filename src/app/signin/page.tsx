e40f33b6715e1d04bcd9c82d5e259f78bd21aea2
phat.do <dotanphat20@gmail.com>
2025-03-29 22:36:57 +0700

    fix: redirect with out render

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -53,20 +53,18 @@ export default function SignInPage() {
         message.success("Login successfully!");
         setUser(data.user);
         setLoading(false);
+
+        if (data?.user.role === "admin") {
+          router.push("/list/lessons");
+        } else if (data?.user.role === "user") {
+          router.push("/");
+        }
       }
     } catch (error) {
       setLoading(false);
       message.error("Login unsuccessfully!");
     }
   };
-
-  useEffect(() => {
-    if (user?.role === "admin") {
-      router.push("/list/lessons");
-    } else if (user?.role === "user") {
-      router.push("/");
-    }
-  }, [router, user]);
 
   const onFinishFailed = (errorInfo: any) => {
     console.log("Failed:", errorInfo);
